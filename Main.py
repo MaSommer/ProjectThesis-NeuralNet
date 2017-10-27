e4c955b8040449fe913028f6953002bc2118f088
MaSommer <martisom@stud.ntnu.no>
2017-10-27 20:48:56 +0200

    parameter fixed

--- Main.py
+++ Main.py
@@ -16,20 +16,20 @@ import ExcelFormatter as excel
 import HyperParamResult as hpr
 
 
-activation_functions = ["tanh", "tanh", "tanh", "relu", "tanh", "sigmoid"]
-hidden_layer_dimension = [500,100]
-time_lags = 3
+activation_functions = ["tanh", "tanh", "tanh", "tanh", "tanh", "tanh"]
+hidden_layer_dimension = [500,40]
+time_lags = 6
 one_hot_vector_interval = [-0.00, 0.00]
 keep_probability_dropout =0.80
 
  #Data set specific
-from_date =  "01.01.2008"
-number_of_trading_days = 2000
+from_date =  "01.01.2012"
+number_of_trading_days = 700
 attributes_input = ["op", "cp"]
 selectedSP500 = ssr.readSelectedStocks("S&P500.txt")
-number_of_networks = 4
+number_of_networks = 1
 epochs = 40
-number_of_stocks =4
+number_of_stocks =100
 
 
  #Training specific
@@ -38,13 +38,15 @@ minibatch_size = 10
 
 rf_rate = 0.02
 
-nr_of_runs = 1
+nr_of_runs = 5
 
-for run_nr in range(1, nr_of_runs+1):
-    time_start = time.time()
-    test = run.Run(activation_functions, hidden_layer_dimension, time_lags, one_hot_vector_interval, number_of_networks, keep_probability_dropout,
-               from_date, number_of_trading_days, attributes_input, number_of_stocks,
-               learning_rate, minibatch_size, epochs, rf_rate, run_nr)
-    test.run_portfolio_in_parallell()
-    time_end = time.time()
-    print("%s seconds ---" % (time_end - time_start))
+
+for time_lag in range(0, time_lags):
+    for run_nr in range(1, nr_of_runs+1):
+        time_start = time.time()
+        test = run.Run(activation_functions, hidden_layer_dimension, time_lag, one_hot_vector_interval, number_of_networks, keep_probability_dropout,
+                   from_date, number_of_trading_days, attributes_input, number_of_stocks,
+                   learning_rate, minibatch_size, epochs, rf_rate, run_nr)
+        test.run_portfolio_in_parallell()
+        time_end = time.time()
+        print("%s seconds ---" % (time_end - time_start))
