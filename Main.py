ed84e8e8a607a132f1619a6023f921a898839a98
Sjur August Waagbø <sjur.waagbo@gmail.com>
2017-10-27 20:36:53 +0200

    Added run nr on terminal output

--- Main.py
+++ Main.py
@@ -29,7 +29,7 @@ attributes_input = ["op", "cp"]
 selectedSP500 = ssr.readSelectedStocks("S&P500.txt")
 number_of_networks = 4
 epochs = 40
-number_of_stocks =100
+number_of_stocks =4
 
 
  #Training specific
@@ -41,7 +41,10 @@ rf_rate = 0.02
 nr_of_runs = 1
 
 for run_nr in range(1, nr_of_runs+1):
+    time_start = time.time()
     test = run.Run(activation_functions, hidden_layer_dimension, time_lags, one_hot_vector_interval, number_of_networks, keep_probability_dropout,
                from_date, number_of_trading_days, attributes_input, number_of_stocks,
                learning_rate, minibatch_size, epochs, rf_rate, run_nr)
     test.run_portfolio_in_parallell()
+    time_end = time.time()
+    print("%s seconds ---" % (time_end - time_start))
