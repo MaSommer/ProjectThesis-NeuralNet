a61509ef3188112b5a385ade409e10b9c3c8750d
MaSommer <martisom@stud.ntnu.no>
2017-11-17 13:08:09 +0100

    Rady for new testing dropout prob

--- Main.py
+++ Main.py
@@ -14,7 +14,7 @@ hidden_layer_dimension = [400,30]
 time_lags_sp = 1
 time_lags_ftse = 10
 one_hot_vector_interval = [-0.000, 0.000]
-keep_probability_dropout = [0.80, 0.50] #first element is input layer and second is hidden layers
+keep_probability_dropout = [1.00, 1.00] #first element is input layer and second is hidden layers
 
  #Data set specific
 from_date =  "01.01.2009"
@@ -42,7 +42,7 @@ selectedSP500 = ssr.readSelectedStocks("S&P500.txt")
 sp500 = pi.InputPortolfioInformation(selectedSP500, attributes_input, from_date, "S&P500_new.txt", 7,
                                      number_of_trading_days, normalize_method="minmax", start_time=time.time())
 
-run_description = "Time lag 0--> and one hot vector 0.000 -> 0.006 with 0.003 in step. Soft label = True"
+run_description = "Keep_prob_dopout is [1.0, 1.0] and one hot vector 0.000 -> 0.006 with 0.003 in step. Soft label = True"
 
 # parser = argparse.ArgumentParser()
 # parser.add_argument('user_name')
@@ -52,12 +52,14 @@ run_description = "Time lag 0--> and one hot vector 0.000 -> 0.006 with 0.003 in
 # username = getattr(usr_pwd,'user_name')
 # pwd = getattr(usr_pwd,'user_pwd')
 
-for time_lag_sp in range(time_lags_sp+1):
+soft_label_list = [True, False]
+
+for soft_label in soft_label_list:
     start_one_hot_interval = [-0.00, 0.00]
     for one_hot_nr in range(3):
         for run_nr in range(1, nr_of_runs+1):
             time_start = time.time()
-            test = run.Run(activation_functions, hidden_layer_dimension, time_lag_sp, time_lags_ftse, start_one_hot_interval, number_of_networks, keep_probability_dropout,
+            test = run.Run(activation_functions, hidden_layer_dimension, time_lags_sp, time_lags_ftse, start_one_hot_interval, number_of_networks, keep_probability_dropout,
                            from_date, number_of_trading_days, attributes_input, number_of_stocks,
                            learning_rate, minibatch_size, epochs, rf_rate, global_run_nr, copy.deepcopy(sp500), soft_label, soft_label_percent, run_description)
             test.run_portfolio_in_parallell()
