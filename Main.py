479cc433a032d5af224bdff87e384abb2c24f7c4
MaSommer <martisom@stud.ntnu.no>
2017-12-16 10:33:46 +0100

    Testing time lags

--- Main.py
+++ Main.py
@@ -32,7 +32,7 @@ minibatch_size = 10
 
 rf_rate = 1.02
 
-nr_of_runs = 3
+nr_of_runs = 5
 global_run_nr = 1
 soft_label = True
 soft_label_percent = 1.0
@@ -65,10 +65,10 @@ h3_start = 0
 
 
 epochs = [40]
-from_dates = ["01.04.2008", "01.08.2008", "01.09.2008", "01.11.2008", "01.12.2008"]
-time_lags_sp = [1, 2, 3, 4]
+from_dates = ["01.04.2008", "15.06.2008", "01.09.2008", "15.11.2008", "01.02.2009"]
+time_lags_sp = [0]
 
-for time_lag_sp in time_lags_sp:
+for from_date in from_dates:
     for epoch in epochs:
         for run_nr in range(1, nr_of_runs+1):
             time_start = time.time()
